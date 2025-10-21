83a55a19037bc90822d35d7ea20f94f5b16b5075
nerealniylubovnik123-droid <nerealniylubovnik123@gmail.com>
2025-10-21 20:02:09 +0300

    с

--- server.cjs
+++ server.cjs
@@ -1,13 +1,10 @@
-import express from "express";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Раздаём статику
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (_, res) => {
